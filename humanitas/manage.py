d62b7a9c19ef91a337d3375abe01c11491317da0
Mateus Zitelli <zitellimateus@gmail.com>
2013-12-25 09:42:46 -0200

    COnfigurado o blog via zinnia

--- humanitas/manage.py
+++ humanitas/manage.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "humanitas.settings")
 
     from django.core.management import execute_from_command_line
-
+    from env_vars import set_env_vars
+    set_env_vars()
     execute_from_command_line(sys.argv)
